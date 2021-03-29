d5ccfd8311e3a0236c118a7a520eb6f4fd180a8a
kccrtv <kimcincocreative@gmail.com>
2021-03-29 16:20:16 -0700

    remove extraneous files for heroku build > > Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com> Co-authored-by: Kim Cinco <kimcincocreative@gmail.com> Co-authored-by: Simão Neves <coday.ink@gmail.com> Co-authored-by: Susana Martins <susana.debug@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
