8a53898c2663f0d65ec9dcae4cff15aedfc7c59e
kccrtv <kimcincocreative@gmail.com>
2021-03-29 16:38:14 -0700

    add favicon > > Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com> Co-authored-by: Kim Cinco <kimcincocreative@gmail.com> Co-authored-by: Simão Neves <coday.ink@gmail.com> Co-authored-by: Susana Martins <susana.debug@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-
-import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
-=======
 
 ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> 9870d19... remove extraneous files for heroku build
