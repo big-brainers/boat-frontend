640b3d9208227419d6366a16a3f87d6ac840b6a9
Nevsimao <coday.ink@gmail.com>
2021-03-30 02:05:03 -0400

    Worked On Sign Up and Log In Pages
    Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com>
    Co-authored-by: Kim Cinco <kimcincocreative@gmail.com>
    Co-authored-by: Susana Martins <susana.debug@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
---
+++ src/landing-components/home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GuestEntrance() {
+	return (
+		<div>Home Page</div>
+	)
+}
