488054085c829298fbc56ca4ea9f2027e7170d79
Nevsimao <coday.ink@gmail.com>
2021-03-30 02:05:03 -0400

    Worked On Sign Up and Log In Pages
    Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com>
    Co-authored-by: Kim Cinco <kimcincocreative@gmail.com>
    Co-authored-by: Susana Martins <susana.debug@gmail.com>

--- src/landing-components/signupFunc.js
+++ src/landing-components/signupFunc.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import {
 	BrowserRouter as Router,
@@ -9,13 +9,8 @@ import {
 import validate from './validate'
 import useForm from './useForm'
 import SignIn from './signin'
-=======
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import validate from './validate';
-import useForm from './UseForm';
-import SignIn from './signin';
->>>>>>> f0687a9... pull pushes for landing page and nav panel components
+
+
 
 const SignupFunc = ({ submitForm }) => {
     const { handleChange, handleSubmit, values, errors } = useForm(
