78bd48a7d00a9f3b4e5198e17dd439dbdd832187
Nevsimao <coday.ink@gmail.com>
2021-03-30 02:05:03 -0400

    Worked On Sign Up and Log In Pages
    Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com>
    Co-authored-by: Kim Cinco <kimcincocreative@gmail.com>
    Co-authored-by: Susana Martins <susana.debug@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -12,3 +13,27 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+<<<<<<< HEAD
+=======
+
+ReactDOM.render(<App />, document.getElementById('root'));
+=======
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+reportWebVitals()
+>>>>>>> d7c9bcc... Worked On Sign Up and Log In Pages
+>>>>>>> 7ce31d5... Worked On Sign Up and Log In Pages
