afb1f990262613e6bb3fa4be889b5945e5030f2e
kccrtv <kimcincocreative@gmail.com>
2021-03-30 11:26:05 -0700

    style sign up page > > Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com> Co-authored-by: Simão Neves <coday.ink@gmail.com> Co-authored-by: Susana Martins <susana.debug@gmail.com>

--- src/landing-components/validate.js
+++ src/landing-components/validate.js
@@ -1,22 +1,22 @@
 export default function validate(values) {
-  let errors = {}
+	let errors = {};
 
-  if (!values.email) {
-    errors.email = 'Email is required'
-  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-    errors.email = 'Email is invalid'
-  }
+	if (!values.email) {
+		errors.email = ' ';
+	} else if (!/\S+@\S+\.\S+/.test(values.email)) {
+		errors.email = 'Email is invalid';
+	}
 
-  if (!values.password) {
-    errors.password = 'Password is required'
-  } else if (values.password.length < 6) {
-    errors.password = 'Password needs to be 6 characters or more'
-  }
+	if (!values.password) {
+		errors.password = ' ';
+	} else if (values.password.length < 6) {
+		errors.password = 'Password needs to be 6 characters or more';
+	}
 
-  if (!values.passconfirm) {
-    errors.passconfirm = 'Password is required'
-  } else if (values.passconfirm !== values.password) {
-    errors.passconfirm = 'Passwords do not match'
-  }
-  return errors
+	if (!values.passconfirm) {
+		errors.passconfirm = ' ';
+	} else if (values.passconfirm !== values.password) {
+		errors.passconfirm = 'Passwords do not match';
+	}
+	return errors;
 }
