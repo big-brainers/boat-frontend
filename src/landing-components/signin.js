38347110f81f30f0c8090d976b359b6bbb2c27f8
kccrtv <kimcincocreative@gmail.com>
2021-03-30 07:01:13 -0700

    update typos in route paths > > Co-authored-by: Simão Neves <coday.ink@gmail.com>

--- src/landing-components/signin.js
+++ src/landing-components/signin.js
@@ -1,48 +1,49 @@
-import React from 'react'
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-  } from "react-router-dom"
-import SignUp from './signup'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import SignUp from './signup';
 
 export default function SignIn() {
 	return (
-	<>
-		<div className='Nav'>
-			<h1>BOAT</h1>
-		</div>
-		<div className='Form'>
-			<form>
-                <h3>Welcome Back!</h3>
-				<h5>Continue With GOOGLE</h5>
-				<h6>or continue with email</h6>
+		<>
+			<div className='Nav'>
+				<h1>BOAT</h1>
+			</div>
+			<div className='Form'>
+				<form>
+					<h3>Welcome Back!</h3>
+					<h5>Continue With GOOGLE</h5>
+					<h6>or continue with email</h6>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Email" />
-                </div>
+					<div className='form-group'>
+						<label>Email</label>
+						<input type='email' className='form-control' placeholder='Email' />
+					</div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Password" />
-                </div>
+					<div className='form-group'>
+						<label>Password</label>
+						<input
+							type='password'
+							className='form-control'
+							placeholder='Password'
+						/>
+					</div>
 
-                <button type="submit" className="btn">Log In</button>
+					<button type='submit' className='btn'>
+						Log In
+					</button>
 
-                <div className="new-member">
-                     <Router>
-						Not yet a member? <Link to='/singUp'>Sign Up</Link>
-						<Switch>
-							<Route path="/signUp">
-								<SignUp />
-							</Route>
-						</Switch>
-					 </Router>
-                </div>
-            </form>
-		</div>
-	</>
-	)
+					<div className='new-member'>
+						<Router>
+							Not yet a member? <Link to='/signUp'>Sign Up</Link>
+							<Switch>
+								<Route path='/signUp'>
+									<SignUp />
+								</Route>
+							</Switch>
+						</Router>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
