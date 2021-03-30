962424e4c96d1805ca02ca1d3e5007534e5e8c82
kccrtv <kimcincocreative@gmail.com>
2021-03-30 11:58:14 -0700

    style welcome page main container

--- src/App.js
+++ src/App.js
@@ -10,43 +10,41 @@ import Schedule from './dashboard/Schedule';
 import Logs from './dashboard/Logs';
 // import SignIn from './landing-components/signin';
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import LandingPage from './landing-components/LandingPage';
 import SignInPage from './components/SignInPage';
 import SignUpPage from './components/SignUpPage';
+import Welcome from './components/Welcome';
 
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 body {
 	font-family: 'Inconsolata', monospace;
+	margin: 0;
 }
 `;
 
 export default function App() {
 	return (
-		<Router>
-			<Link to='/'>Home</Link>
-			<Link to='/signin'>Sign In</Link>
-			<Link to='/signup'>Sign Up</Link>
+		<>
+			<Router>
+				<Link to='/'>Home</Link>
+				<Link to='/signin'>Sign In</Link>
+				<Link to='/signup'>Sign Up</Link>
 
-			<Switch>
-				<Route exact path='/'>
-					<LandingPage />
-				</Route>
-				<Route path='/signin'>
-					<SignInPage />
-				</Route>
-				<Route path='/signUp'>
-					<SignUp />
-				</Route>
-				<Route path='/dashboard'>
-					<Dashboard />
-				</Route>
-				<Route path='/schedule'>
-					<Schedule />
-				</Route>
-				<Route path='/logs'>
-					<Logs />
-				</Route>
-			</Switch>
-		</Router>
+				<GlobalStyle />
+				<Switch>
+					<Route exact path='/'>
+						<LandingPage />
+					</Route>
+					<Route path='/signin'>
+						<SignInPage />
+					</Route>
+					<Route path='/signup'>
+						<SignUpPage />
+					</Route>
+				</Switch>
+			</Router>
+			<Welcome />
+		</>
 	);
 }
