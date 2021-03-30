ccb3e6dc002f98569f524cb908eea1448873b714
kccrtv <kimcincocreative@gmail.com>
2021-03-30 11:58:14 -0700

    style welcome page main container

--- src/App.js
+++ src/App.js
@@ -11,21 +11,27 @@ import Schedule from './components/dashboard/Schedule';
 import Logs from './components/dashboard/Logs';
 import Overview from './components/dashboard/Overview';
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import LandingPage from './landing-components/LandingPage';
+import SignInPage from './components/SignInPage';
+import SignUpPage from './components/SignUpPage';
+import Welcome from './components/Welcome';
+
 const GlobalStyle = createGlobalStyle`
 body {
 	font-family: 'Inconsolata', monospace;
 	margin: 0;
-	height: 100vh;
-	background-color: #e1effa;
-	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='2000' height='240' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23e5e7f0' stroke-width='5.8' stroke-opacity='0.37'%3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E");
 }
 `;
 
 export default function App() {
 	return (
 		<>
-			<GlobalStyle />
 			<Router>
+				<Link to='/'>Home</Link>
+				<Link to='/signin'>Sign In</Link>
+				<Link to='/signup'>Sign Up</Link>
+
 				<GlobalStyle />
 				<Switch>
 					<Route exact path='/'>
@@ -42,6 +48,7 @@ export default function App() {
 					</Route>
 				</Switch>
 			</Router>
+			<Welcome />
 		</>
 	);
 }
