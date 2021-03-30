35689c2c10c8a85203595f1984c680a1a1d24048
Ria Schwalbe <riaschwalbe@Rias-MacBook-Pro-17018.local>
2021-03-30 13:08:52 -0700

    added JoinUs & LearnMore componenets. routes to sign in and sign up added to Nav.js > > Co-authored-by: Jalen Foreman <jalenforeman594@gmail.com> Co-authored-by: Kim Cinco <kimcincocreative@gmail.com> Co-authored-by: Simão Neves <coday.ink@gmail.com> Co-authored-by: Susana Martins <susana.debug@gmail.com>

---
+++ src/landing-components/JoinUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+  function JoinUs() {
+    
+  return (
+    <div>
+        <button onClick="">Join Us</button>
+    </div>
+  
+  )
+}
+
+export default JoinUs
---
+++ src/landing-components/Nav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import SignUp from './signup';
+import SignIn from './signin';
+import styled from 'styled-components';
+
+const NavBar = styled.nav`
+	background-color: #f2f2f2;
+	color: #222f65;
+	height: 70px;
+	padding-bottom: 45px;
+`;
+
+// const SecondaryButton = styled.secondaryButton`
+//   color: #111b47;
+//   border-radius: 2px;
+// `;
+
+// const PrimaryButton = styled.primaryButton`
+//   background: #111b47;
+//   border-radius: 2px;
+//   color: FFFFFF;
+// `;
+
+const Nav = () => {
+	return (
+		<div>
+			{/* //having issues with styled components */}
+			<NavBar>
+				<h2> BOAT </h2>
+				{/* <button onClick="">Sign In </button>
+        <button onClick="">Sign Up </button> */}
+				<Route path='/signIn'>
+					<SignIn />
+				</Route>
+				<Route path='/signUp'>
+					<SignUp />
+				</Route>
+			</NavBar>
+		</div>
+	);
+};
+
+export default Nav;
