e4044621325f3a71bc0d5f9fd481a3deebfd1048
kccrtv <kimcincocreative@gmail.com>
2021-04-01 17:43:24 -0700

    style nav panel with navlinks

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ export default function App(props) {
 					<Route path='/logs/:id' component={Entry} />
 					<Route path='/compose' component={Compose} />
 				</Switch>
-			</Router>
+			</Router>{' '}
+			*/}
 		</>
 	);
 }
