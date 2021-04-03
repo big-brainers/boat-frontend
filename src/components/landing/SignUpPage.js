9568cfa1f1112af36bc038236b23b64ccf2a08eb
Nevsimao <coday.ink@gmail.com>
2021-04-03 16:27:42 -0400

    nothing much

--- src/components/landing/SignUpPage.js
+++ src/components/landing/SignUpPage.js
@@ -1,7 +1,7 @@
 import React, { useState, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import SignUpFunc from './SignUpFunc';
+import SignUpFunc from './signupFunc';
 import SignInPage from './SignInPage';
 
 const SignInNav = styled.nav`
